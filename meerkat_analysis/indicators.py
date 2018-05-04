5ade1d062c5002d3497b3fbeba3192c9f4baab1c
Jonathan Berry <jonathan.s.berry@googlemail.com>
2018-05-04 17:45:42 +0100

    Debug typo

--- meerkat_analysis/indicators.py
+++ meerkat_analysis/indicators.py
@@ -8,7 +8,7 @@ def fix_dates(start_date, end_date, epi_week_start_day):
     """
     We parse the start and end date and remove any timezone information
 
-    Args: 
+    Args:
        start_date: start date
        end_date: end_date
        epi_week_start_day: what day of the week to start the timeline(Mon=0)
@@ -23,7 +23,7 @@ def fix_dates(start_date, end_date, epi_week_start_day):
                                       hour=0, second=0,
                                       minute=0,
                                       microsecond=0)
-        
+
     if epi_week_start_day is None:
         epi_week_start_day = start_date.weekday()
     if end_date:
@@ -34,7 +34,7 @@ def fix_dates(start_date, end_date, epi_week_start_day):
         if offset < 0:
             offset = 7 + offset
         end_date = end_date - timedelta(days=offset + 1)
-        
+
     freqs = ["W-MON", "W-TUE", "W-WED", "W-THU", "W-FRI", "W-SAT", "W-SUN"]
     freq = freqs[epi_week_start_day]
 
@@ -45,7 +45,7 @@ def count(data, var_id, start_date=None, end_date=None, epi_week_start_day=None)
     """
     We return the total count of var_id and a timeline by epi_week
 
-    Args: 
+    Args:
         data: data in dataframe
         var_id: the variable id to count
         start_date: start date
@@ -58,7 +58,6 @@ def count(data, var_id, start_date=None, end_date=None, epi_week_start_day=None)
     start_date, end_date, freq = fix_dates(start_date,
                                            end_date,
                                            epi_week_start_day)
-    print(level)
     dates = pd.date_range(start_date, end_date, freq=freq, closed="left")
 
     data = data[data["date"] >= start_date]
@@ -68,15 +67,15 @@ def count(data, var_id, start_date=None, end_date=None, epi_week_start_day=None)
         pd.TimeGrouper(key="date", freq=freq, label="left", closed="left")).sum()[var_id]
     timeline = timeline.reindex(dates).fillna(0)
     return (total, timeline)
-    
+
 def count_over_count(data, numerator_id, denominator_id, start_date=None, end_date=None, epi_week_start_day=None, restrict=False):
     """
     We return the total proportion of numerator_id over denominator_id and a timeline by epi_week
 
-    Args: 
+    Args:
         data: data in dataframe
         denominator: the denominator id
-        numerator: the numerator_id 
+        numerator: the numerator_id
         start_date: start date
         end_date: end_date
         epi_week_start_day: what day of the week to start the timeline(Mon=0)
@@ -88,7 +87,7 @@ def count_over_count(data, numerator_id, denominator_id, start_date=None, end_da
 
     if restrict:
         data = data[data[restrict] == 1]
-    
+
     start_date, end_date, freq = fix_dates(start_date,
                                            end_date,
                                            epi_week_start_day)
@@ -104,13 +103,13 @@ def count_over_count(data, numerator_id, denominator_id, start_date=None, end_da
         proportion = data[numerator_id].count() / data[denominator_id].count()
         #        ci = proportion.proportion_confint(data[numerator_id].sum(), data[denominator_id].sum(), method="wilson")
 
-    
+
     timeline = data.groupby(
         pd.TimeGrouper(key="date", freq=freq, label="left", closed="left")).count()
     timeline = timeline.reindex(dates).fillna(0)
     timeline.loc[timeline[denominator_id] == 0, denominator_id] = 1
     proportion_timeline = timeline[numerator_id] / timeline[denominator_id]
-    
+
     return (proportion, proportion_timeline)
 
 
@@ -120,7 +119,7 @@ def number_per_week_clinic(data, variable, locations,
                            drop_duplicates=True):
     """
     Returns the number of variable per week taking start_date into account
-    Args: 
+    Args:
         data: data in dataframe
         var_id: the variable id to count
         locations: location class
@@ -131,13 +130,13 @@ def number_per_week_clinic(data, variable, locations,
        clinic_timeline: all clinics with a timeline
     """
 
-    
+
     today = datetime.now()
     locs = locations.locations
     start_date, end_date, freq = fix_dates(start_date,
                                            end_date,
                                            epi_week_start_day)
-    
+
     # We drop duplicates so each clinic can only have one record per day
     if drop_duplicates:
         data = data.drop_duplicates(subset=["region", "district", "clinic", "date", variable])
@@ -171,7 +170,7 @@ def clinic_to_level(data, locations, level, cutoff_per_week=None):
     """
     Transform clinic data to data on a higher level
 
-    Args: 
+    Args:
        data: Data Frame with clinic data
        locations: Location class
        level: district, region or country
@@ -198,7 +197,7 @@ def number_of_sites(data, level, start_date=None, end_date=None,
     start_date, end_date, freq = fix_dates(start_date,
                                            end_date,
                                            epi_week_start_day)
-    
+
     dates = pd.date_range(start_date, end_date, freq=freq, closed="left")
 
     data = data[data["date"] >= start_date]
@@ -239,7 +238,7 @@ def grouped_count_over_count(data, numerator, denominator, restrict=False,
     Returns:
        dataframe: With a row for each group by
     """
-    
+
     clinics = pd.DataFrame(columns=["score", "N"] + fields, index=[group_by])
     for name, group in data.groupby(group_by):
         if np.sum(group[denominator]) > 0:
@@ -260,7 +259,7 @@ def plot_level_total(data, locations, level,  cutoff_per_week=None):
     """
     Transform clinic data to data on a higher level and plots it
 
-    Args: 
+    Args:
        data: Data Frame with clinic data
        locations: Location class
        level: district, region or country
@@ -290,14 +289,14 @@ def plot_level_total(data, locations, level,  cutoff_per_week=None):
     red_lower = [-10, -10]
     ax.fill_between(x, red_upper, red_lower, facecolor="red", alpha=0.5)
 
-            
-    
+
+
 
 def plot_multilevel_timeline(data):
     """
     Plots multilevel timeline
 
-    Args: 
+    Args:
        data: data to plot
     """
     f = pylab.figure()

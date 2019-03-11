6e123893ff3226e150b74237eff308f6f58fd3e8
gulfa <gunnarroe@gmail.com>
2019-03-11 15:22:23 +0200

    Try for matplotlib imports

--- meerkat_analysis/__init__.py
+++ meerkat_analysis/__init__.py
@@ -1,6 +1,10 @@
-import pylab
-import os
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
+try:
+    import pylab
+    import os
 
-pylab.style.use(dir_path + "/style/meerkat.mplstyle")
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+
+    pylab.style.use(dir_path + "/style/meerkat.mplstyle")
+except ImportError:
+    pass
